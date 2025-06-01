13a25df3e701e41cea0a0086b73b7ab1b072698b
Matari73 <marianadeoliveira73@hotmail.com>
2025-06-01 18:34:10 -0300

    Adding initial seed

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import { sequelize, connectInDatabase } from './db/db.js';
 import routes from './routes/index.js';
+import seed from './scripts/seed.js';
 import './models/associations.js';
 
 const app = express();
 routes(app);
 
 const startApp = async () => {
-  await connectInDatabase();
-  await sequelize.sync({ force: true }); // recria todas as tabelas
-  console.log('Banco de dados sincronizado');
+  try {
+    await connectInDatabase();
+    await sequelize.sync({ force: true }); // Sincronização das tabelas
+
+    console.log('Banco de dados sincronizado. Rodando seed...');
+    await seed(); // só roda o seed depois que as tabelas foram criadas
+
+    console.log('Seed finalizado. Aplicação pronta.');
+  } catch (err) {
+    console.error('Erro ao iniciar a aplicação:', err);
+  }
 };
 
 startApp();
