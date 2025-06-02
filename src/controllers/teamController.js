eb2258df9d3e539c2be389586b564c42a15d1262
GiovanaHoffmann <giovana.hoffmann@hotmail.com>
2025-06-02 01:47:18 -0300

    fix: remove 'ranking' from Team

--- src/controllers/teamController.js
+++ src/controllers/teamController.js
@@ -134,6 +134,7 @@ export const validateTeam = async (req, res) => {
       valid: validation.valid,
       message: validation.message
     });
+    
   } catch (error) {
     res.status(500).json({
       valid: false,
