e08a9e501df8d0f484dd473432cb988dc6748d95
Matari73 <marianadeoliveira73@hotmail.com>
2025-06-20 17:31:35 -0300

    Adding swagger configs

---
+++ src/routes/swagger.js
@@ -0,0 +1,30 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Valorant API',
+      version: '1.0.0',
+      description: 'Documentação da API de gerenciamento de campeonatos Valorant',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3001',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+export default swaggerSpec;
